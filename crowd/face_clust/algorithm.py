8ddf9e604a2a0498ec1f146770d85eb83b36aa89
shravankumar9892 <shravankumarshetty9892@gmail.com>
2020-04-24 00:39:51 +0530

    package renamed

--- crowd/face_clust/algorithm.py
+++ crowd/face_clust/algorithm.py
@@ -1,4 +1,4 @@
-from pyfacy.utils import load_images_to_clust_encodings,save_faces,selecting_clust_alg_model
+from crowd.utils import load_images_to_clust_encodings,save_faces,selecting_clust_alg_model
 import os
 
 class Face_Clust_Algorithm():
